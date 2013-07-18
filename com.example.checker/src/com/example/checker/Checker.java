73f771a83fbe51c37aaddac0e2312c166fed613a
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-07-18 12:34:00 +0200

    02 - Run and indicate via UnsupportedOperationException

--- com.example.checker/src/com/example/checker/Checker.java
+++ com.example.checker/src/com/example/checker/Checker.java
@@ -3,6 +3,9 @@
  */
 package com.example.checker;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * @author markus
  * 
@@ -27,6 +30,18 @@ public class Checker {
 	 *            The base url where to start from
 	 */
 	private void check(final String string) {
-		// TODO Implement
+		// Try to convert the String parameter into a URL. This might fail and
+		// thus we have to handle the MUE. In case of said exception we notify
+		// the user and exit our little program right away.
+		URL url = null;
+		try {
+			url = new URL(string);
+		} catch (MalformedURLException e) {
+			System.err.println("Invalid url given");
+			System.exit(1);
+		}
+
+		System.out.println(String.format("Going to start checker on url: %s",
+				url.toString()));
 	}
 }
