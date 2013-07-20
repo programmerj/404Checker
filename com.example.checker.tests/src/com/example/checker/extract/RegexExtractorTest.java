9a2a973e7a72420cc38378b4282869f8231d92dc
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-07-20 12:09:13 +0200

    13a - Create regular expression for URLs and test with a unit test

---
+++ com.example.checker.tests/src/com/example/checker/extract/RegexExtractorTest.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright 2013 me, under Beerware license.
+ */
+package com.example.checker.extract;
+
+import static org.junit.Assert.assertTrue;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+import com.example.checker.extract.RegexExtractor;
+
+/**
+ * @author markus
+ * 
+ */
+public class RegexExtractorTest {
+
+	@Test
+	public void testValid() throws MalformedURLException {
+		final List<String> valid = new ArrayList<String>();
+		valid.add("http://www.vogella.com");
+		valid.add("https://github.com/sbrosinski/RegexTester");
+		valid.add("http://www.bastian-bergerhoff.com/eclipse/features/web/QuickREx/toc.html");
+		valid.add("http://docs.oracle.com/javase/tutorial/essential/regex/");
+		valid.add("http://www.vogella.com/articles/JavaRegularExpressions/article.html");
+		valid.add("http://www.vogella.com/articles/JavaRegularExpressions/article.html#top");
+
+		valid.add("http://username:pass@www.vogella.com/articles/JavaRegularExpressions/article.html");
+
+		valid.add("ftp://ftp.osuosl.org/pub/eclipse/eclipse/downloads/drops4/");
+		valid.add("ftp://ftp.osuosl.org/pub/eclipse/eclipse/downloads/drops4/../");
+
+		// verify that we indeed test valid urls
+		for (String input : valid) {
+			new URL(input);
+		}
+
+		// really test the regex
+		for (String input : valid) {
+			assertTrue(String.format("Failed to match %s", input),
+					Pattern.matches(RegexExtractor.regex, input));
+		}
+	}
+
+	@Test
+	public void testInvalid() {
+		final List<String> invalid = new ArrayList<String>();
+		invalid.add("httb://github.com/sbrosinski/RegexTester");
+		// TODO Add more invalid examples
+
+		// verify that we indeed test invalid urls
+		for (String input : invalid) {
+			try {
+				new URL(input);
+			} catch (MalformedURLException e) {
+				// expected
+				continue;
+			}
+			throw new RuntimeException(String.format(
+					"input %s has to be invalid!", input));
+		}
+
+		// really test the regex
+		for (String input : invalid) {
+			assertTrue(String.format("Incorrectly matched %s", input),
+					!Pattern.matches(RegexExtractor.regex, input));
+		}
+	}
+}
---
+++ com.example.checker/src/com/example/checker/extract/RegexExtractor.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2013 me, under Beerware license.
+ */
+package com.example.checker.extract;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Set;
+
+/**
+ * @author markus
+ * 
+ */
+public class RegexExtractor implements IExtractor {
+
+	// package private to allow access to unit tests
+	static final String regex = "^(https?|ftp)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.example.checker.extract.IExtractor#extractLinks(java.net.URL,
+	 * java.util.Set)
+	 */
+	@Override
+	public void extractLinks(URL url, Set<URL> links) throws IOException {
+		// TODO implement
+		throw new UnsupportedOperationException("not yet implemented");
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.example.checker.extract.IExtractor#getContentType()
+	 */
+	@Override
+	public String getContentType() {
+		// TODO implement
+		return null;
+	}
+}
