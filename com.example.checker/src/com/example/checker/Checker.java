acbb2806b5b169a78831447e52432f0e9ef9ed35
Markus Alexander Kuppe <kuppe-github.com@lemmster.de>
2013-07-22 07:04:44 +0200

    25a - Use pool of threads to concurrently recurse into link checking to max out system resource (Synchronization done via AtomicInteger)(MockedCheckerTest broken)

--- com.example.checker/src/com/example/checker/Checker.java
+++ com.example.checker/src/com/example/checker/Checker.java
@@ -12,6 +12,9 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.example.checker.extract.HTMLRegexExtractor;
 import com.example.checker.extract.IExtractor;
@@ -28,12 +31,17 @@ import com.example.checker.extract.XMLExtractor;
  */
 public class Checker {
 
+	private static final int NUM_THREADS = 4;
+
 	private int threshold = Integer.getInteger(getClass().getName()
 			+ ".threshold", 2);
 
 	/**
-	 * The depth the checker has reached up until now during dead link detection
+	 * The depth the checker has reached up until now during dead link
+	 * detection. Inconsistent reads are acceptable here as it is just used for
+	 * reporting (we can tolerate to report a slightly incorrect level).
 	 */
+	// XXX What about volatile here?
 	private int maxDepth = 0;
 
 	/**
@@ -74,6 +82,17 @@ public class Checker {
 	 * result page.
 	 */
 	private String authority;
+
+	private final ThreadPoolExecutor threadPool = (ThreadPoolExecutor) Executors
+			.newFixedThreadPool(NUM_THREADS);
+
+	/**
+	 * Count work (links) not yet explored. Initialized with 1 to prevent main
+	 * thread from exiting immediately after spawning the first worker thread
+	 * (before it even had a chance to extract the first set of links) and the
+	 * final decrement to 0 in {@link Checker#check(Set)}
+	 */
+	private AtomicInteger undone = new AtomicInteger(1);
 
 	public Checker() {
 		extractors = new HashMap<String, IExtractor>();
@@ -120,24 +139,32 @@ public class Checker {
 		final Set<Link> urls = new HashSet<Link>();
 		urls.add(new URLLink(url));
 
+		/* initial configuration done */
+
 		// Create a worker thread that forks of from the main thread to do link
 		// detection. The main thread will move on to the next loop and report
 		// progress every five seconds.
-		final Thread worker = new Thread(new Runnable() {
+		threadPool.execute(new Runnable() {
 			@Override
 			public void run() {
 				check(urls);
 			}
-		}, "Link Checker Worker Thread");
-		worker.start();
+		});
 
-		while (worker.isAlive()) {
+		// Check if any work (links) are left undone.
+		while (undone.get() > 0) {
 			System.out
 					.println(String
 							.format("Processed %d pages at depth %d, found %d dead links so far",
 									seen.size(), maxDepth, dead.size()));
 			Thread.sleep(5000);
 		}
+
+		// Need to shut down the thread pool explicitly. Otherwise the VM never
+		// terminates and leaves the threads open.
+		threadPool.shutdown();
+
+		/* final reportig below here */
 
 		// Loop over the (now sorted) list of urls and print them to stdout
 		System.out.println(String.format("==== Scanned %d pages ====",
@@ -156,25 +183,20 @@ public class Checker {
 
 	Set<Link> check(final Set<Link> urls) {
 		check(urls, dead, 0);
+		undone.decrementAndGet();
 		return dead;
 	}
 
-	private void check(final Set<Link> urls, Set<Link> dead, int level) {
+	private void check(final Set<Link> urls, final Set<Link> dead, int level) {
 		// Example of ternary operator (syntactical sugar for if/else)
 		// Logically it increases maxDepth if the current level is deeper
 		maxDepth = (maxDepth < level) ? level : maxDepth;
 
 		if (level++ > threshold) {
+			undone.addAndGet(urls.size() * -1);
 			return;
 		}
 		for (Link url : urls) {
-			// Websites logically are equivalent to a _cyclic_ graph, thus a
-			// list of seen vertices is used to turn it into a DAG.
-			// Using the add op has the nice advantage that the set operation
-			// becomes atomic (see putIfAbsent).
-			if (!this.seen.add(url)) {
-				continue;
-			}
 
 			// links is used to store all extracted urls from the web page
 			// A TreeSet implements SortedSet and thus it automagically sorts
@@ -183,40 +205,76 @@ public class Checker {
 			final Set<Link> links = new HashSet<Link>();
 
 			try {
-				// Each content type needs a specific Extractor. E.g. html uses
-				// a
-				// line based pattern matching looking for hrefs, whereas an xml
-				// file could navigate the xml structure to extract links
-				final String contentType = url.getContentType();
-				if (contentType == null) {
-					// 404
-					dead.add(url);
-				} else if (!url.isSameAuthority(authority)) {
-					// (Partially redundant)
-					// if we get here we know _without_ parsing the web page
-					// that it is alive (because we managed to determine the
-					// content type) and thus can skip it (no need to extract
-					// its page links)
+				// Websites logically are equivalent to a _cyclic_ graph, thus a
+				// list of seen vertices is used to turn it into a DAG.
+				// Using the add op has the nice advantage that the set operation
+				// becomes atomic (see putIfAbsent).
+				if (!this.seen.add(url)) {
 					continue;
-				} else if (extractors.containsKey(contentType)) {
-					IExtractor iExtractor = extractors.get(contentType);
-					iExtractor.extractLinks(url, links);
-				} else {
-					unknownContentType.add(contentType);
 				}
-			} catch (IOException | IllegalArgumentException e) {
-				// 404 (if https connection fails when not connected to
-				// internet)
-				dead.add(url);
-			}
-			// Do not follow out-bound links from the page where we started on.
-			// E.g. don't check google for 404s. Still check if the link itself
-			// is correct
-			if (!url.isSameAuthority(authority)) {
-				continue;
+				try {
+					// Each content type needs a specific Extractor. E.g. html uses
+					// a
+					// line based pattern matching looking for hrefs, whereas an xml
+					// file could navigate the xml structure to extract links
+					final String contentType = url.getContentType();
+					if (contentType == null) {
+						// 404
+						dead.add(url);
+					} else if (!url.isSameAuthority(authority)) {
+						// (Partially redundant)
+						// if we get here we know _without_ parsing the web page
+						// that it is alive (because we managed to determine the
+						// content type) and thus can skip it (no need to extract
+						// its page links)
+						continue;
+					} else if (extractors.containsKey(contentType)) {
+						IExtractor iExtractor = extractors.get(contentType);
+						iExtractor.extractLinks(url, links);
+						// Here is a slight chance of early termination
+						// when
+						// the main thread checks undone immediately
+						// extractLinks(..) returns but before undone is
+						// incremented. This is accounted for by 
+						// decrementing undone in try catch
+						undone.addAndGet(links.size());
+					} else {
+						unknownContentType.add(contentType);
+						continue;
+					}
+				} catch (IOException | IllegalArgumentException e) {
+					// 404 (if https connection fails when not connected to
+					// internet)
+					dead.add(url);
+				}
+				// Do not follow out-bound links from the page where we started on.
+				// E.g. don't check google for 404s. Still check if the link itself
+				// is correct
+				if (!url.isSameAuthority(authority)) {
+					continue;
+				}
+			} finally {
+				// Decrement has to happen after new work have been added to the
+				// work list. Otherwise we terminate too early.
+				undone.decrementAndGet();
 			}
 			// recurse into
-			check(links, dead, level);
+
+			// If threads are available in the pool, use one to recurse into the
+			// list. If no thread is available, use the current one.
+			// This reassembles depth-first search as we don't half the list
+			// first and spawn two new Runnables on the same level.
+			if (threadPool.getActiveCount() < NUM_THREADS) {
+				final int foo = level;
+				threadPool.execute(new Runnable() {
+					@Override
+					public void run() {
+						check(links, dead, foo);
+					}
+				});
+			} else {
+				check(links, dead, level);
+			}
 		}
 	}
 
